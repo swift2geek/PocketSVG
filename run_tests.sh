2f5c88ce8b518462e2e34f7d9cdea1b7f6b0c2bc
Ariel Elkin <arielelkin@gmail.com>
2018-04-04 23:50:29 +0200

    update travis

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,11 @@
+## this script builds the iOS and macOS frameworks and demos, and
+## runs the tests on iOS. It is called from Travis CI (via .travis.yml)
+## and placed in a separate file for better readability.
+
 IPHONE6SIM="platform=iOS Simulator,name=iPhone 6,OS=11.2"
 
 ## build iOS framework:
+set -o pipefail && \
 xcodebuild \
   -project PocketSVG.xcodeproj \
   -scheme "PocketSVG (iOS)" \
@@ -8,6 +13,7 @@ xcodebuild \
   clean build | xcpretty
 
 ## build macOS framework:
+set -o pipefail && \
 xcodebuild \
   -project PocketSVG.xcodeproj \
   -scheme "PocketSVG (Mac)" \
@@ -15,6 +21,7 @@ xcodebuild \
   clean build | xcpretty
 
 ## build iOS demo:
+set -o pipefail && \
 xcodebuild \
   -workspace Demos/Demos.xcworkspace \
   -destination "$IPHONE6SIM" \
@@ -22,6 +29,7 @@ xcodebuild \
   clean build | xcpretty
 
 ## build macOS demo:
+set -o pipefail && \
 xcodebuild \
   -workspace Demos/Demos.xcworkspace \
   -destination "arch=x86_64" \
@@ -29,6 +37,7 @@ xcodebuild \
   clean build | xcpretty
 
 ## run tests:
+set -o pipefail && \
 xcodebuild \
   -project PocketSVG.xcodeproj \
   -scheme PocketSVGTests \
