7864f7319411445ea598c9640f8dc3e32281e3be
Ariel Elkin <arielelkin@gmail.com>
2018-04-04 23:22:43 +0200

    update travis

--- run_tests.sh
+++ run_tests.sh
@@ -1,34 +1,36 @@
+IPHONE6SIM="platform=iOS Simulator,name=iPhone 6,OS=11.2"
+
 ## build iOS framework:
 xcodebuild \
   -project PocketSVG.xcodeproj \
   -scheme "PocketSVG (iOS)" \
   -destination "$IPHONE6SIM" \
-  clean build
+  clean build | xcpretty
 
 ## build macOS framework:
 xcodebuild \
   -project PocketSVG.xcodeproj \
   -scheme "PocketSVG (Mac)" \
   -destination "arch=x86_64" \
-  clean build
+  clean build | xcpretty
 
 ## build iOS demo:
 xcodebuild \
   -workspace Demos/Demos.xcworkspace \
   -destination "$IPHONE6SIM" \
   -scheme Demo-iOS \
-  clean build
+  clean build | xcpretty
 
 ## build macOS demo:
 xcodebuild \
   -workspace Demos/Demos.xcworkspace \
   -destination "arch=x86_64" \
   -scheme Demo-macOS \
-  clean build
+  clean build | xcpretty
 
 ## run tests:
 xcodebuild \
   -project PocketSVG.xcodeproj \
   -scheme PocketSVGTests \
   -destination "$IPHONE6SIM" \
-  test
+  test | xcpretty
